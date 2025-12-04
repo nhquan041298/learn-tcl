c797f4301d14699834b2bec63f091a2ef01b8b10
nhquan041298 <74725395+nhquan041298@users.noreply.github.com>
2025-12-04 10:22:40 +0700

    Implement file reading and existence check
    Added a script to read a file and check its existence.

--- parse_upf_viewdefinition.tcl
+++ parse_upf_viewdefinition.tcl
@@ -1,1 +1,21 @@
+#!/bin/sh
+
+#\
+exec tclsh "$0" ${1+"$@"}
+
+proc READ_FILE { {FILE_NAME} } {
+    set chk [file exists $FILE_NAME]
+    if {$chk==0} {
+        puts "* ERROR: $FILE_NAME not found."
+        return -1
+    }
+
+    set fid [open $FILE_NAME]
+    set CHECK_LIST {}
+    while {[gets $fid str]>=0} {
+        lappend CHECK_LIST $str
+    }
+    close $fid
+    return $CHECK_LIST
+}
 
